75fd4c4d3304188547a14a28f093c1f7148399c3
Luke W <luke.waldren@mecca.com.au>
2024-03-03 08:47:50 +1100

    landing page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { LandingLamp } from "../components/ui/lamp";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full">
+      <LandingLamp />
+    </main>
+  );
+}
