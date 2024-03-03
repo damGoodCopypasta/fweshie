db750d843c56ff24f238ccbe1eeb1ebf50351ab3
Luke W <luke.waldren@gmail.com>
2024-03-03 13:37:37 +1100

    use svh instead

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { LandingLamp } from "../components/ui/lamp";
 export default function Home() {
   return (
-    <main className="flex w-full" style={{height: '100vh'}}>
+    <main className="flex w-full" style={{height: '100svh'}}>
       <LandingLamp />
     </main>
   );
